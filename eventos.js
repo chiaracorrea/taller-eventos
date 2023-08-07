51f97de4ed3ecc2119935fe6704f4bbab4b87239
chiaracorrea <chiaractasan@gmail.com>
2023-08-06 23:06:11 -0300

    eventos 1
    taller de eventos

---
+++ eventos.js
@@ -0,0 +1,12 @@
+function mostrarAlerta() {
+    alert("Hola!");
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const divElement = document.getElementById("miDiv");
+    divElement.addEventListener("click", function() {
+        alert("Hola! Soy el div");
+    });
+});
+
+
